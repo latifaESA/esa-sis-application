49fd8f2cddb906dfb3e0f6337233c0c092a33a15
Ali <mroueh_ali@outlook.com>
2023-05-14 09:57:40 +0300

    ..

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -101,7 +101,7 @@ export const authOptions = {
                
               // check if the user completed the survey
                 try {
-                let {data} = await axios.get('https://survey.esa.edu.lb/BPI/PathwayService.svc/PWGetUserPreventAccess?pathway=140&userid=201705636', {
+                let {data} = await axios.get('https://survey.esa.edu.lb/BPI/PathwayService.svc/PWGetUserPreventAccess?pathway=140&userid=201705637', {
                 httpsAgent: new https.Agent({
                   rejectUnauthorized: false,
                 })
@@ -110,7 +110,7 @@ export const authOptions = {
                 // if the user did not complete the survey then send the links
                 if(data.blocked){
                   try{
-                  let {data} = await axios.get('https://survey.esa.edu.lb/BPI/PathwayService.svc/PWBlueTasks?pathway=140&userid=201705636&SubjectIDs=2022_EMBA-CC-08_01,2022_EMBA-S-04_01,2022_EMBA-EC-03_02,2022_EMBA-EC-09_01', {
+                  let {data} = await axios.get('https://survey.esa.edu.lb/BPI/PathwayService.svc/PWBlueTasks?pathway=140&userid=201705637&SubjectIDs=2022_EMBA-CC-08_01,2022_EMBA-S-04_01,2022_EMBA-EC-03_02,2022_EMBA-EC-09_01', {
                     httpsAgent: new https.Agent({
                       rejectUnauthorized: false,
                     })
