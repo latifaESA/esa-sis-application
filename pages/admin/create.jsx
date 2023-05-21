c02869ced8a2ee3225531b034437ef03aa448bc9
Eng. KANSO Adi <eng.kansoadi@gmail.com>
2023-05-21 18:29:10 +0300

    enhancement

--- pages/admin/create.jsx
+++ pages/admin/create.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import ProfileScreen from '../../components/Dashboard/profile';
+// import ProfileScreen from '../../components/Dashboard/profile';
 
 export default function Profile() {
   return (
@@ -8,12 +8,11 @@ export default function Profile() {
         <title>SIS Admin - Create Page</title>
       </Head>
 
-      <p className='text-gray-700 text-3xl pt-5 mb-10 font-bold'>
-      Create
+      <p className="text-gray-700 text-3xl pt-5 mb-10 font-bold">
+        Create Acoount
       </p>
 
-      <div className='grid lg:grid-cols-1 gap-5 mb-5'>
-      </div>
+      <div className="grid lg:grid-cols-1 gap-5 mb-5"></div>
     </>
   );
 }
