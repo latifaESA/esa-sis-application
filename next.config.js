c90961ac2920512233094f82f8ae09894935444c
Eng. KANSO Adi <eng.kansoadi@gmail.com>
2023-06-13 08:58:26 +0300

    bugs

--- next.config.js
+++ next.config.js
@@ -9,11 +9,7 @@ const nextConfig = {
     // domains: ['res.cloudinary.com'],
     domains: ['localhost','res.cloudinary.com', '80.77.180.245'],
 
-    // domains: ['res.cloudinary.com'],
-    domains: ['localhost','res.cloudinary.com', '80.77.180.245'],
-
-
-  },
+     },
 
   swcMinify: true,
   serverRuntimeConfig: {
