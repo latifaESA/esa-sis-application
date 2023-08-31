a69a8b4bf55e233e7a5300173142f3bd5973264a
Batoolhareb <111172611+Batoolhareb@users.noreply.github.com>
2023-08-31 12:55:43 +0300

    ViewStudents

--- components/Dashboard/DashboardComps/StudentView.jsx
+++ components/Dashboard/DashboardComps/StudentView.jsx
@@ -34,144 +34,29 @@ const StudentView = () => {
   const router = useRouter();
   return (
     <>
-// <<<<<<< Ali
-//      <div className='flex flex-col'>
-//             {/* Main Section */}
-//             <Link href='/student/main'>
-//               <div
-//                 className={`pl-2 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
-//                   router.pathname == '/student/main'
-//                     ? 'bg-blue-100 text-blue-500'
-//                     : 'text-gray-400 hover:bg-blue-100 hover:text-blue-500'
-//                 }`}
-//               >
-//                 <div className='mr-2'>
-//                   <ComputerDesktopIcon className='h-5 w-5' />
-//                 </div>
-//                 <div>
-//                   <p>Basic Information</p>
-//                 </div>
-//               </div>
-//             </Link>
-
-//             {/* Financial Section */}
-
-//             <Link href='/student/financial'>
-//               <div
-//                 className={`pl-2 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
-//                   router.pathname == '/student/classes'
-//                     ? 'bg-blue-100 text-blue-500'
-//                     : 'text-gray-400 hover:bg-blue-100 hover:text-blue-500'
-//                 }`}
-//                 disabled={true}
-//               >
-//                 <div className='mr-2'>
-//                   <TableCellsIcon className='h-5 w-5' />
-//                 </div>
-//                 <div>
-//                   <p>Financial</p>
-//                 </div>
-//               </div>
-//             </Link>
-
-//             {/* Schedule Section */}
-//           { !isLimited && <Link href='/student/grades'>
-//               <div
-//                 className={`pl-2 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
-//                   router.pathname == '/student/grades'
-//                     ? 'bg-blue-100 text-blue-500'
-//                     : 'text-gray-400 hover:bg-blue-100 hover:text-blue-500'
-                    
-//                 }`}
-                
-//               >
-//                 <div className='mr-2' >
-//                   <AcademicCapIcon  className='h-5 w-5' />
-//                 </div>
-//                 <div >
-//                   <p>Grades</p>
-//                 </div>
-//               </div>
-//             </Link>}
-            
-//             { isLimited &&
-
-//               <div
-//                 className={`pl-2 py-3 mx-5 rounded pointer-events-none text-center cursor-pointer mb-3 flex items-center transition-colors ${
-//                   router.pathname == '/student/grades'
-//                     ? 'bg-blue-100 text-blue-500'
-//                     : 'text-gray-400 hover:bg-blue-100 hover:text-blue-500'
-                    
-//                 }`}
-                
-//               >
-//                 <div className='mr-2' >
-//                   <AcademicCapIcon  className='h-5 w-5' />
-//                 </div>
-//                 <div className='flex'>
-//                   <p>Grades </p><LockClosedIcon className='h-4 w-4 ml-2'/>
-//                 </div>
-//               </div>
-//             }
-
-//             {/* Grades Section */}
-
-//             <Link href='/student/schedule'>
-//               <div
-//                 className={`pl-2 py-3 mx-5 rounded text-center  cursor-pointer mb-3 flex items-center transition-colors ${
-//                   router.pathname == '/student/schedule'
-//                     ? 'bg-blue-100 text-blue-500'
-//                     : 'text-gray-400 hover:bg-blue-100 hover:text-blue-500'
-//                 }`}
-//               >
-//                 <div className='mr-2'>
-//                   <CalendarDaysIcon className='h-5 w-5' />
-//                 </div>
-//                 <div>
-//                   <p>Schedule</p>
-//                 </div>
-//               </div>
-//             </Link>
-
-//             {/* Attendance Section */}
-//             <Link href='/student/attendance'>
-//               <div
-//                 className={`pl-2 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
-//                   router.pathname == '/student/attendance'
-//                     ? 'bg-blue-100 text-blue-500'
-//                     : 'text-gray-400 hover:bg-blue-100 hover:text-blue-500'
-//                 }`}
-//               >
-//                 <div className='mr-2'>
-//                   <CreditCardIcon className='h-5 w-5' />
-//                 </div>
-//                 <div>
-//                   <p>Attendance</p>
-//                 </div>
-//               </div>
-//             </Link>
-// =======
       <div className='flex flex-col'>
 
 
         {/* Classes Section */}
 
-        <Link href='/student/classes'>
-          <div
-            className={`pl-2 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == '/student/classes'
-                ? 'bg-blue-100 text-blue-500'
-                : 'text-gray-400 hover:bg-blue-100 hover:text-blue-500'
-              }`}
-            disabled={true}
-          >
-            <div className='mr-2'>
-              <TableCellsIcon className='h-5 w-5' />
-            </div>
-            <div>
-              <p>Financial</p>
-            </div>
-          </div>
-        </Link>
+        <Link href='/student/financial'>
+              <div
+                className={`pl-2 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                  router.pathname == '/student/classes'
+                    ? 'bg-blue-100 text-blue-500'
+                    : 'text-gray-400 hover:bg-blue-100 hover:text-blue-500'
+                }`}
+                disabled={true}
+              >
+                <div className='mr-2'>
+                  <TableCellsIcon className='h-5 w-5' />
+                </div>
+                <div>
+                  <p>Financial</p>
+                </div>
+              </div>
+            </Link>
+
 
         
 
@@ -209,7 +94,7 @@ const StudentView = () => {
             </div>
           </div>
         </Link>
-// >>>>>>> main
+
 
         {/* Payment Section */}
 
