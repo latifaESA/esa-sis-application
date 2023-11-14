24672254e22703e97024e557b852ad43019437b4
hassanchakkour <hassan.chakkour@hotmail.com>
2023-11-14 16:55:41 +0200

    email before class notification

---
+++ pages/api/pmApi/addNotification.js
@@ -0,0 +1,30 @@
+const { connect, disconnect } = require("../../../utilities/db");
+const { insertNotifications } = require("../controller/queries");
+
+// const axios = require('axios')
+// import https from 'https';
+
+async function handler(req, res) {
+  try {
+    const connection = await connect();
+
+    const { receiverIds, senderId, content, subject } = req.body;
+    console.log(receiverIds, content, subject, senderId);
+    const data = await insertNotifications(
+      connection,
+      receiverIds,
+      senderId,
+      content,
+      subject
+    );
+    await disconnect(connection);
+
+    return res.status("200").send(data);
+  } catch (error) {
+    // console.log('the error is: ', error)
+    return res.status("401").send(error);
+    // return error;
+  }
+}
+// export default handler;
+module.exports = handler;
