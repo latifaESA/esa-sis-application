2656b44c6c594461f7f4ec563daa41a562da3900
Batoolhareb <111172611+Batoolhareb@users.noreply.github.com>
2023-11-20 19:26:54 +0200

    testGrades

--- pages/programManager/grades.jsx
+++ pages/programManager/grades.jsx
@@ -8,6 +8,8 @@ import GradeList from '../../components/Dashboard/GradesList';
 import DownloadGrades from './downloadGrades';
 import UploadGrades from './uploadGrades';
 import axios from 'axios';
+import GradeListGMP from '../../components/Dashboard/GradeListGMP';
+import GradeListRTF from '../../components/Dashboard/GradeListRTF';
 export default function Grades() {
     const { data: session } = useSession();
     const [clickDownload, setClickDownload] = useState(false)
@@ -19,6 +21,7 @@ export default function Grades() {
     const [PromotionName, setPromotionName] = useState([])
     const [GPA, setGPA] = useState("")
     const [Rank, setRank] = useState("")
+    const [gradeOver30, setGradesOver30] = useState("")
     const [grade, setGrades] = useState("")
     const [courseId, setCourseId] = useState("")
     const [users, setUser] = useState([])
@@ -35,6 +38,33 @@ export default function Grades() {
     const upload = () => {
         setClickUpload(true)
     }
+
+    // Function to extract the first word before a hyphen "-"
+    const getFirstWordBeforeHyphen = (text) => {
+        if (text) {
+            const words = text.split("-");
+            if (words.length > 0) {
+                return words[0];
+            }
+        }
+        return "";
+    };
+
+    // Function to extract the first word before a hyphen "-"
+    const getFirstWordAfterHyphen = (text) => {
+        if (text) {
+            const words = text.split("-");
+            if (words.length > 0) {
+                return words[1];
+            }
+        }
+        return "";
+    };
+
+    const firstMajorWord = getFirstWordBeforeHyphen(session?.user.majorName);
+    const secondMajorWord = getFirstWordAfterHyphen(session?.user.majorName);
+
+    const isExeMajor = firstMajorWord === "EXED";
     const fetchPromotion = async () => {
         try {
             const payload = {
@@ -54,13 +84,13 @@ export default function Grades() {
                 table: 'gpa',
             }
             const response = await axios.post('/api/pmApi/getAll', payload)
-       
+
             setData(response.data.rows)
         } catch (error) {
             return error
         }
     }
-    console.log(data)
+    // console.log(data)
     const showAll = async () => {
         try {
             const payload = {
@@ -111,11 +141,120 @@ export default function Grades() {
             return error
         }
     }
+
+    const showAllGMP = async () => {
+        try {
+            const payload = {
+                student_id: '',
+                first_name: '',
+                last_name: '',
+                promotion: '',
+                major_id: session.user?.majorid,
+                course_id: '',
+                grades: '',
+                task_name: ''
+            }
+            const response = await axios.post('/api/pmApi/filterGradeGMP', payload)
+
+            setUser(response.data.data)
+            setStudentID("")
+            setStudentFirstName("")
+            setStudentLast("")
+            setGrades("")
+            setCourseId("")
+            setPromotion("")
+            setTaskName("")
+        } catch (error) {
+            return error
+        }
+    }
+    const searchGMP = async () => {
+        try {
+            const payload = {
+                student_id: studentId,
+                first_name: studentFirstName,
+                last_name: studentLastName,
+                promotion: promotions,
+                major_id: session.user?.majorid,
+                course_id: courseId,
+                grades: grade,
+                task_name: taskName
+            }
+
+            const response = await axios.post('/api/pmApi/filterGradeGMP', payload)
+
+            setUser(response.data.data)
+        } catch (error) {
+            return error
+        }
+    }
+
+    const showAllRTF = async () => {
+        try {
+            const payload = {
+                student_id: '',
+                first_name: '',
+                last_name: '',
+                promotion: '',
+                major_id: session.user?.majorid,
+                course_id: '',
+                grades_over_20: '',
+                grades_over_30: '',
+                task_name: ''
+            }
+            const response = await axios.post('/api/pmApi/filterRTF', payload)
+
+            setUser(response.data.data)
+            setStudentID("")
+            setGradesOver30('')
+            setStudentFirstName("")
+            setStudentLast("")
+            setGrades("")
+            setCourseId("")
+            setPromotion("")
+            setTaskName("")
+        } catch (error) {
+            return error
+        }
+    }
+    const searchRTF = async () => {
+        try {
+            const payload = {
+                student_id: studentId,
+                first_name: studentFirstName,
+                last_name: studentLastName,
+                promotion: promotions,
+                major_id: session.user?.majorid,
+                course_id: courseId,
+                grades_over_20: grade,
+                grades_over_30: gradeOver30,
+                task_name: taskName
+            }
+
+            const response = await axios.post('/api/pmApi/filterRTF', payload)
+
+            setUser(response.data.data)
+        } catch (error) {
+            return error
+        }
+    }
+
     useEffect(() => {
-        fetchGPA()
-        fetchPromotion()
-        showAll()
-        search()
+        if (!isExeMajor) {
+            fetchGPA()
+            fetchPromotion()
+            showAll()
+            search()
+        } else if (isExeMajor && secondMajorWord === 'GMP') {
+            fetchPromotion()
+            showAllGMP()
+            searchGMP()
+        } else if (isExeMajor && secondMajorWord === 'Digital Transformation in Financial Services') {
+            fetchPromotion()
+            showAllRTF()
+            searchRTF()
+        }
+
     }, [])
 
     return (
@@ -129,7 +268,8 @@ export default function Grades() {
                     <p className="text-gray-700 text-3xl pt-5 mb-10 font-bold">Grades</p>
                     <form>
                         {clickDownload && <DownloadGrades setClickDownload={setClickDownload} />}
-                        {clickUpload && <UploadGrades setClickUpload={setClickUpload} showAll={showAll} />}
+                        {clickUpload && <UploadGrades setClickUpload={setClickUpload} showAll={showAll}
+                            showAllGMP={showAllGMP} showAllRTF={showAllRTF} />}
 
                         <div className="mb-4 md:mb-12 p-6">
                             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -228,44 +368,64 @@ export default function Grades() {
                                         className="w-full"
                                     />
                                 </div>
-                                <div className="flex items-center">
-                                    <label className="mr-2">GPA:</label>
-                                    <select
-                                        className="w-full"
-                                        value={GPA}
-                                        onChange={(e) => setGPA(e.target.value)}
-                                    >
-                                        <option value="">GPA</option>
-                                        {data.length > 0 ? (
-                                            data.map((item, index) => (
-                                                <option key={index} value={item.gpa}>
-                                                    {item.gpa}
-                                                </option>
-                                            ))
-                                        ) : (
-                                            <option value={""}>NO GPA</option>
-                                        )}
-                                    </select>
-                                </div>
-                                <div className="flex items-center">
-                                    <label className="mr-2">Rank:</label>
-                                    <select
-                                        className="w-full"
-                                        value={Rank}
-                                        onChange={(e) => setRank(e.target.value)}
-                                    >
-                                        <option value="">Rank</option>
-                                        {data.length > 0 ? (
-                                            data.map((item, index) => (
-                                                <option key={index} value={item.rank}>
-                                                    {item.rank}
-                                                </option>
-                                            ))
-                                        ) : (
-                                            <option value={""}>NO Rank</option>
-                                        )}
-                                    </select>
-                                </div>
+                                {isExeMajor && secondMajorWord === 'Digital Transformation in Financial Services' ?
+                                    <div className="flex items-center">
+                                        <label className="mr-2">Grade Over 30:</label>
+                                        <input
+                                            type="text"
+                                            value={gradeOver30}
+                                            onChange={(e) => setGradesOver30(e.target.value)}
+                                            placeholder="Grade Over 30"
+                                            className="w-full"
+                                        />
+                                    </div>
+                                    : <></>}
+                                {!isExeMajor ? <>
+
+                                    <div className="flex items-center">
+                                        <label className="mr-2">GPA:</label>
+                                        <select
+                                            className="w-full"
+                                            value={GPA}
+                                            onChange={(e) => setGPA(e.target.value)}
+                                        >
+                                            <option value="">GPA</option>
+                                            {data.length > 0 ? (
+                                                data.map((item, index) => (
+                                                    <option key={index} value={item.gpa}>
+                                                        {item.gpa}
+                                                    </option>
+                                                ))
+                                            ) : (
+                                                <option value={""}>NO GPA</option>
+                                            )}
+                                        </select>
+                                    </div>
+                                </> : <></>}
+                                {!isExeMajor ? <>
+
+                                    <div className="flex items-center">
+                                        <label className="mr-2">Rank:</label>
+                                        <select
+                                            className="w-full"
+                                            value={Rank}
+                                            onChange={(e) => setRank(e.target.value)}
+                                        >
+                                            <option value="">Rank</option>
+                                            {data.length > 0 ? (
+                                                data.map((item, index) => (
+                                                    <option key={index} value={item.rank}>
+                                                        {item.rank}
+                                                    </option>
+                                                ))
+                                            ) : (
+                                                <option value={""}>NO Rank</option>
+                                            )}
+                                        </select>
+                                    </div>
+                                </> : <></>}
+
+
                                 <div className="flex items-center">
                                     <label className="mr-2">Task:</label>
                                     <select
@@ -276,9 +436,11 @@ export default function Grades() {
                                         <option value="">Task</option>
                                         <option value='assignment'>assignment</option>
                                         <option value='exam'>exam</option>
+                                        <option value='project'>project</option>
                                     </select>
                                 </div>
                             </div>
+
 
 
                         </div>
@@ -288,21 +450,31 @@ export default function Grades() {
                             <button
                                 className="primary-button rounded btnCol invisible max-[850px]:visible max-[850px]:hidden text-white hover:text-white hover:font-bold"
                                 type="button"
-                                onClick={showAll}
+                                onClick={!isExeMajor ? showAll : showAllGMP}
                             >
                                 Show All
                             </button>
                             <button
                                 className="primary-button rounded btnCol text-white hover:text-white hover:font-bold"
                                 type="button"
-                                onClick={search}
+                                onClick={!isExeMajor ? search :
+                                    (secondMajorWord === 'GMP' ?
+                                        searchGMP :
+                                        isExeMajor && secondMajorWord === 'Digital Transformation in Financial Services'
+                                            ? searchRTF : <></>)
+                                }
                             >
                                 Search
                             </button>
                             <button
                                 className="primary-button rounded btnCol text-white hover:text-white hover:font-bold"
                                 type="button"
-                                onClick={showAll}
+                                onClick={!isExeMajor ? showAll :
+                                    (secondMajorWord === 'GMP' ?
+                                        showAllGMP :
+                                        isExeMajor && secondMajorWord === 'Digital Transformation in Financial Services'
+                                            ? showAllRTF : <></>)
+                                }
                             >
                                 Show All
                             </button>
@@ -324,7 +496,15 @@ export default function Grades() {
                                 Upload Grade
                             </button>
                         </div>
-                        <GradeList users={users} setUser={setUser} />
+                        {
+                            !isExeMajor ? <GradeList users={users} setUser={setUser} />
+                                : isExeMajor && secondMajorWord === 'GMP' ? <GradeListGMP users={users} setUser={setUser} />
+                                    : isExeMajor && secondMajorWord === 'Digital Transformation in Financial Services' ?
+                                        <GradeListRTF users={users} setUser={setUser} />
+                                        : <></>
+
+                        }
+
                     </form>
                 </>
             ) : (
