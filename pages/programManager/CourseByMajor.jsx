8b1744d83d648315178ab0bd6b240c4ad3d5ef65
AliMroueh <mroueh_ali@outlook.com>
2024-02-12 10:13:06 +0200

    res exed in multi

--- pages/programManager/CourseByMajor.jsx
+++ pages/programManager/CourseByMajor.jsx
@@ -295,7 +295,7 @@ export default function CourseByMajor() {
 
 
                 : <></>}
-              <div className="flex flex-col min-[850px]:flex-row gap-4">
+              <div className="flex flex-col min-[850px]:flex-row gap-4 min-[850px]:col-start-2 min-[1100px]:col-start-3 h-10 max-[850px]:mb-16">
                 <button
                   className="primary-button btnCol text-white w-60 hover:text-white hover:font-bold"
                   type="button"
@@ -312,7 +312,7 @@ export default function CourseByMajor() {
                 </button>
               </div>
               {isExeMajor ? <>
-                <div className="flex flex-col min-[850px]:flex-row gap-4">
+                <div className="flex flex-col min-[850px]:flex-row gap-4 min-[850px]:col-start-2 min-[1100px]:col-start-3 h-10 max-[850px]:mb-16">
                 <button
                   className="primary-button btnCol text-white w-60 hover:text-white hover:font-bold"
                   type="button"
