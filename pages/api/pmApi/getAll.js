d284f30ba60a8ba3dfd077cd669d34a142ac4882
AliMroueh <mroueh_ali@outlook.com>
2024-03-01 10:03:15 +0200

    new update on class

--- pages/api/pmApi/getAll.js
+++ pages/api/pmApi/getAll.js
@@ -16,7 +16,7 @@ async function handler(req, res) {
     return res.status("200").send(data);
   } catch (error) {
     // console.log('the error is: ', error)
-    return res.status("401").send(error);
+    return res.status("500").send(error);
     // return error;
   }
 }
--- pages/api/pmApi/getuserteacher.js
+++ pages/api/pmApi/getuserteacher.js
@@ -1,4 +1,4 @@
-const { connect } = require("../../../utilities/db");
+const { connect, disconnect } = require("../../../utilities/db");
 const { getUserTeacher } = require("../controller/queries");
 
 // const axios = require('axios')
@@ -9,10 +9,11 @@ async function handler(req, res) {
     const connection = await connect();
     const { pmID } = req.body;
     const data = await getUserTeacher(connection, pmID);
+    await disconnect(connection);
     return res.status("200").send(data);
   } catch (error) {
     // console.log('the error is: ', error)
-    return res.status("401").send(error);
+    return res.status("500").send(error);
     // return error;
   }
 }
