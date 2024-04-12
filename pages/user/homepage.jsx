a28c368d701b158647ecb86f9c5e4a45e3090d80
Batoolhareb <111172611+Batoolhareb@users.noreply.github.com>
2024-04-12 19:08:07 +0300

    homepagetest

---
+++ pages/user/homepage.jsx
@@ -0,0 +1,95 @@
+// import Link from 'next/link';
+import React from "react";
+// import { useForm } from "react-hook-form";
+// import { getError } from "../../utilities/error";
+// import { signIn, useSession, getSession } from "next-auth/react";
+// import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
+// import selection_data from '../../utilities/selection_data';
+// import decrypt from "../../utilities/encrypt_decrypt/decryptText";
+// import { useState } from "react";
+// import { useDispatch } from "react-redux";
+import esaBuilding from "../../public/images/ESA3.jpg";
+import esaLogo from "../../public/images/esa.png";
+
+// import Person2Icon from '@mui/icons-material/Person2';
+// import LockIcon from '@mui/icons-material/Lock';
+import Image from "next/image";
+
+
+export default function HomeScreen() {
+
+
+    return (
+        <>
+            <Head>
+                <title>ESA SIS - Login</title>
+                <link rel="icon" href="/public/index.ico" />
+            </Head>
+            <div className="bg-[#F7F7F7] h-screen">
+                <div className="flex justify-center   text-black">
+                    <div className="imageRes lg:visible invisible">
+                        <Image
+                            src={esaBuilding}
+                            alt="Esa-Building"
+                            width={456}
+                            height={690}
+                        />
+                    </div>
+                    <div className="bg-white pr-11 sm:pl-11">
+                        <div className="log flex pr-11 ">
+                            <Image
+                                src={esaLogo}
+                                width={81}
+                                className="esaLogo"
+                                alt="Logo"
+                                height='100%'
+                            />
+                            <div className="leading-7 welcometoSis mt-4 ml-5">
+                                <p className="welcomeTo mt-11">WELCOME TO</p>
+                                <span className="SIS">Student Information System (SIS)</span>
+                            </div>
+                        </div>
+                        <div className="contentSIS">
+                            <div className="SISText">This site will allow you to:</div>
+                            <div className="ContainerCircle">
+                                <div className="circle blue">
+                                    <p className="ContentText">Check your grades</p>
+                                </div>
+                                <div className="circle gray">
+                                    <p className="ContentText">
+                                        Get Notifications
+
+                                    </p>
+                                </div>
+                                <div className="circle lightBlue">
+                                    <p className="ContentText">
+                                        Submit Requests
+                                    </p>
+                                </div>
+                                <div className="circle darkBlue">
+                                    <p className="ContentText">
+                                        Check your payment
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Link href="/user/login" legacyBehavior>
+                            <div className="text-center login text-[#3D709A] text-[39px] bold cursor-pointer">
+                                Login
+                            </div>
+                        </Link>
+                        <div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+HomeScreen.getLayout = function (page) {
+    return <>{page}</>;
+};
